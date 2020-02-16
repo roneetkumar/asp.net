cc471b647cc7460fe33573d8e2aa6bb95b2aaae2
Roneet Kumar <roneetparida@gmail.com>
2020-02-16 15:12:15 -0500

    update

---
+++ SMTIDatabase/SMTIWebsite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SMTIWebsite.MvcApplication" Language="C#" %>
---
+++ SMTIDatabase/SMTIWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SMTIWebsite.Startup))]
+namespace SMTIWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
